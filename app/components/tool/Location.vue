eea42d85d1d6fd0e6ae6b515e9ea56c3916fff9e
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-09-03 18:59:44 +0200

    feat: update Location component styles and improve localization message clarity

--- app/components/tool/Location.vue
+++ app/components/tool/Location.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="md:max-w-2/3 shadow-lg rounded-xl border border-gray-200 dark:border-gray-700 overflow-hidden relative z-10">
       <NuxtImg class="rounded-xl" src="/location.png" />
-      <div class="size-14 rounded-full border-2 border-black absolute z-50 top-2/5 -translate-y-1/2 left-1/5 -translate-x-1/2 animate-bounce">
+      <div class="size-12 rounded-full border-2 border-sky-500 absolute z-50 top-2/5 -translate-y-1/2 left-1/5 -translate-x-1/2 animate-bounce">
         <NuxtImg src="/arthur pro.webp" class="rounded-full" />
       </div>
     </div>
