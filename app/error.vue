ccd23859e26489d3ee3f0c0dba14703c6ab82842
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-09-03 16:23:50 +0200

    feat: remove HomeLink component from error page for cleaner UI

--- app/error.vue
+++ app/error.vue
@@ -10,7 +10,6 @@ const { t } = useI18n()
       </h1>
       <p class="text-center flex gap-0.5">
         {{ t('error.main') }}
-        <HomeLink label="home" href="/" />
         <NuxtLink href="/" class="sofia flex items-center group">
           <span class="duration-300 underline-offset-2 font-semibold text-md text-black dark:text-white underline decoration-gray-300 dark:decoration-neutral-700 group-hover:decoration-black dark:group-hover:decoration-white">
             {{ t('error.redirect') }}
