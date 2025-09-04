38c55ff812b84c4212ba2051cb96f2e1b6c0a4d6
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-09-04 16:34:05 +0200

    feat: integrate locale head for improved SEO and accessibility

--- app/app.vue
+++ app/app.vue
@@ -8,10 +8,24 @@ useHead({
 
 const route = useRoute()
 const { messages } = useChatStore()
+const head = useLocaleHead()
 </script>
 
 <template>
   <UApp>
+    <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
+      <Head>
+        <template v-for="link in head.link" :key="link.key">
+          <Link :id="link.key" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in head.meta" :key="meta.key">
+          <Meta :id="meta.key" :property="meta.property" :content="meta.content" />
+        </template>
+      </Head>
+      <Body>
+        <slot />
+      </Body>
+    </Html>
     <NuxtLoadingIndicator color="#808080" />
     <AppBackground />
     <UContainer>
