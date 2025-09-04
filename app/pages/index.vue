4217eb6c1c3ffc66f0d2bcac10b42f4e7a769983
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-09-04 16:43:51 +0200

    fix: remove hardcoded IDs from ChatMessageContainer for improved flexibility

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -29,7 +29,6 @@ watch(
     <ChatMain />
     <div ref="parents" class="space-y-4 md:my-32 mb-16">
       <ChatMessageContainer
-        :id="99999999998"
         v-motion
         :initial="{
           opacity: 0,
@@ -55,7 +54,6 @@ watch(
         }"
       />
       <ChatMessageContainer
-        :id="99999999999"
         v-motion
         :initial="{
           opacity: 0,
