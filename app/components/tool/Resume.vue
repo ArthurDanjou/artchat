590628fea2051067ce9ae83b2ecb5772ca7aff53
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-09-05 14:33:48 +0200

    feat: update resume component with localized file names and add PDF files

--- app/components/tool/Resume.vue
+++ app/components/tool/Resume.vue
@@ -1,6 +1,21 @@
 <script lang="ts" setup>
-const { t, locale } = useI18n()
-const date = (date: string) => useDateFormat(new Date(date), 'D MMM YYYY - hh:mm', { locales: locale.value ?? 'en' })
+const { t } = useI18n()
+
+interface ResumeFile {
+  name: string
+  file: string
+}
+
+const resumes = ref<ResumeFile[]>([
+  {
+    name: t('tool.resume.files.en'),
+    file: '/resumes/CV M1 2025.pdf',
+  },
+  {
+    name: t('tool.resume.files.fr'),
+    file: '/resumes/CV M1 2025 Français.pdf',
+  },
+])
 </script>
 
 <template>
@@ -8,29 +23,27 @@ const date = (date: string) => useDateFormat(new Date(date), 'D MMM YYYY - hh:mm
     <p class="prose dark:prose-invert">
       {{ t('tool.resume.main') }}
     </p>
-    <div class="flex gap-4 m-1">
-      <UCard variant="outline" class="md:max-w-1/2 shadow-sm bg-white dark:bg-neutral-900" :ui="{ body: 'flex justify-between items-center gap-4' }">
-        <UCard class="rounded-sm" :ui="{ body: 'p-2 sm:p-2 flex items-center justify-center' }">
-          <UIcon name="i-ph-file-pdf-duotone" size="48" />
-        </UCard>
-        <div>
-          <p>File 1</p>
-          <p class="text-muted">
-            {{ t('tool.resume.uploaded') }} {{ date('2025-01-01') }}
+    <div class="flex flex-wrap gap-4 m-1">
+      <NuxtLink
+        v-for="resume in resumes"
+        :key="resume.file"
+        :to="resume.file"
+        target="_blank"
+        class="md:max-w-1/2"
+      >
+        <UCard
+          variant="outline"
+          class="w-full shadow-sm bg-white dark:bg-neutral-900"
+          :ui="{ body: 'flex justify-between items-center gap-4' }"
+        >
+          <UCard class="rounded-sm" :ui="{ body: 'p-2 sm:p-2 flex items-center justify-center' }">
+            <UIcon name="i-ph-file-pdf-duotone" size="48" />
+          </UCard>
+          <p>
+            {{ resume.name }}
           </p>
-        </div>
-      </UCard>
-      <UCard variant="outline" class="md:max-w-1/2 shadow-sm bg-white dark:bg-neutral-900" :ui="{ body: 'flex justify-between items-center gap-4' }">
-        <UCard class="rounded-sm" :ui="{ body: 'p-2 sm:p-2 flex items-center justify-center' }">
-          <UIcon name="i-ph-file-pdf-duotone" size="48" />
         </UCard>
-        <div>
-          <p>File 2</p>
-          <p class="text-muted">
-            {{ t('tool.resume.uploaded') }} {{ date('2025-01-01') }}
-          </p>
-        </div>
-      </UCard>
+      </NuxtLink>
     </div>
   </section>
 </template>
